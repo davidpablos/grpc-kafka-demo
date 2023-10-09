1ae3168213034cf63c747c637a5bc040ac80ee7a
David Pablos <david.pablos@diversius.com>
2023-10-09 22:46:33 +0200

    user-service-grpc module added

---
+++ user-service-grpc/README.md
@@ -0,0 +1,3 @@
+# user-service
+
+mvn clean package
---
+++ user-service-grpc/src/main/java/com/dpablos/grpckafkademo/user/UserServiceGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.dpablos.grpckafkademo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceGrpcApplication.class, args);
+    }
+
+}
